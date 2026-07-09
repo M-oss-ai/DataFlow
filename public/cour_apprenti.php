b9318906a261677a242b51928d48862c1308c0be
Linux User <user@linux.org>
2026-07-09 15:19:27 +0200

    message

--- public/cour_apprenti.php
+++ public/cour_apprenti.php
@@ -11,7 +11,7 @@
       <div>
         <a href="/test.php?page=journal" class="menu">Accueil</a>
         <a href="/test.php?page=module" class="menu">les Modules</a>
-        <a href="/test.php?page=cour_apprenti" class="menu">les Cours</a>
+        <a href="/test.php?page=cour" class="menu">les Cours</a>
       </div>
       <div>
         <p><?= $user["name"] ?></p>
