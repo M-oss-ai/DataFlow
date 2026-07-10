fc215d35c23d18d746d12c5e431a0c98b3e1b85e
Linux User <user@linux.org>
2026-07-10 08:05:19 +0200

    10.07.2026 0805

---
+++ public/header_formateur.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <title>DataFlow</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header class="entete">
+      <div>
+        <a href="/test.php?page=journal" class="menu">Accueil</a>
+        <a href="/test.php?page=add_module" class="menu">Ajouter un module</a>
+        <a href="/test.php?page=module" class="menu">les Modules</a>
+        <a href="/test.php?page=add_course" class="menu">Ajouter un Cours</a>
+        <a href="/test.php?page=cour" class="menu">les Cours</a>
+      </div>
+      <div>
+        <?= $user["name"] ?>
+        <a href="#" class="menu">Se déconnecter</a>
+      </div>
+    </header>
+  </body>
+</html>
